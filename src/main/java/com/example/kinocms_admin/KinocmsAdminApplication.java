3c6da74a5e4bf6e13cca1950f5228c0b446d5aa3
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-05-27 21:15:09 +0300

    creating a project

---
+++ src/main/java/com/example/kinocms_admin/KinocmsAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinocms_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinocmsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinocmsAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kinocms_admin/KinocmsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinocms_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinocmsAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
