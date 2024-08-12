fd125f0c731ceaa042966b97b881661b655a3ad2
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-08-13 00:22:43 +0300

    page response class

--- src/main/java/com/example/kinocms_admin/model/PageResponse.java
+++ src/main/java/com/example/kinocms_admin/model/PageResponse.java
@@ -1,21 +1,16 @@
 package com.example.kinocms_admin.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.Value;
 
 import org.springframework.data.domain.Page;
-
 import java.util.List;
-
 @Value
 public class PageResponse<T> {
     List<T> content;
     Metadata metadata;
 
     public static <T> PageResponse<T> of(Page<T> page) {
-        var metadata = new Metadata(page.getNumber(), page.getSize(), page.getTotalElements());
+        var metadata = new Metadata(page.getNumber(), page.getSize(), page.getTotalElements(),page.getTotalPages());
         return new PageResponse<>(page.getContent(), metadata);
     }
 
@@ -24,5 +19,6 @@ public class PageResponse<T> {
         int page;
         int size;
         long totalElements;
+        long totalPages;
     }
 }
