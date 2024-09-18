0f65395f21599b028cecae2e48851899dd93073a
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-09-19 02:32:35 +0300

    delete users creating from initialize project

--- src/main/java/com/example/kinocms_admin/service/serviceimp/DatabaseLoader.java
+++ src/main/java/com/example/kinocms_admin/service/serviceimp/DatabaseLoader.java
@@ -16,17 +16,13 @@ import java.util.List;
 public class DatabaseLoader implements CommandLineRunner {
     private final PageServiceImp pageServiceImp;
 
-    private final UserServiceImp userServiceImp;
-
-    public DatabaseLoader(PageServiceImp pageServiceImp, UserServiceImp userServiceImp) {
+    public DatabaseLoader(PageServiceImp pageServiceImp) {
         this.pageServiceImp = pageServiceImp;
-        this.userServiceImp = userServiceImp;
     }
 
     @Override
     public void run(String... args) throws Exception {
         List<Page> all = pageServiceImp.getAll();
-        List<User> users = userServiceImp.getAll();
         if (all.isEmpty()) {
             Page pageMain = new Page(true, PageType.main, LocalDate.now());
             Page pageMobile = new Page(true, PageType.mobile, LocalDate.now());
@@ -68,29 +64,6 @@ public class DatabaseLoader implements CommandLineRunner {
             pageServiceImp.save(pageChild);
             pageServiceImp.save(pageContact);
             pageServiceImp.save(pageMobile);
-        }
-        if (users.isEmpty()) {
-            User user1 = new User("roma", "pravnyk", "zidan", "0985545991", "roomich@gmail.com", "Lviv", "1234", "-", LanguageCode.Ukr, true, LocalDate.of(2003, 12, 28), LocalDate.now(), false, "Шкільна 108/2");
-            User user2 = new User("andriy", "fedorovych", "n1oit", "0666790166", "programlearn2023@gmail.com", "Kyiv", "1111", "-", LanguageCode.Ukr, true, LocalDate.of(2003, 12, 7), LocalDate.now(), false, "Костопільська 2");
-            User user3 = new User("masha", "melnyk", "marka", "0888123466", "englandnew2023@gmail.com", "Zhytomyr", "2222", "-", LanguageCode.Eng, false, LocalDate.of(2002, 5, 15), LocalDate.now(), false, "Шаршарова 2в");
-            User user4 = new User("dasha", "levina", "povstanets", "0985386941", "dashaLevina@gmail.com", "Lviv", "3333", "-", LanguageCode.Eng, false, LocalDate.of(2004, 3, 2), LocalDate.now(), false, "Гетьман 2/1");
-            User user5 = new User("rostyslave", "dub", "marsianin", "0866785670", "rost@gmail.com", "Kyiv", "4455", "-", LanguageCode.Eng, true, LocalDate.of(2004, 7, 6), LocalDate.now(), false, "Дружня 22d");
-            User user6 = new User("katya", "gorodchuk", "katyagorodchuk", "0889765891", "katka@gmail.com", "Lviv", "6554", "-", LanguageCode.Eng, false, LocalDate.of(2004, 7, 6), LocalDate.now(), false, "Патріот 52");
-            User user7 = new User("andriy", "fedorovych", "n1oit", "0666790166", "andrios@gmail.com", "Kharkiv", "1111", "-", LanguageCode.Ukr, true, LocalDate.of(2003, 12, 7), LocalDate.now(), false, "Костопільська 2");
-            User user8 = new User("katya", "gorodchuk", "katyagorodchuk", "0889765891", "katka@gmail.com", "Kyiv", "6554", "-", LanguageCode.Eng, false, LocalDate.of(2004, 7, 6), LocalDate.now(), false, "Патріот 52");
-            User user9 = new User("dasha", "levina", "povstanets", "0985386941", "dashaLevina@gmail.com", "Rivne", "3333", "-", LanguageCode.Eng, false, LocalDate.of(2004, 3, 2), LocalDate.now(), false, "Гетьман 2/1");
-            User user10 = new User("rostyslave", "dub", "marsianin", "0866785670", "rost@gmail.com", "Kyiv", "4455", "-", LanguageCode.Eng, true, LocalDate.of(2004, 7, 6), LocalDate.now(), false, "Дружня 22d");
-
-            userServiceImp.save(user1);
-            userServiceImp.save(user2);
-            userServiceImp.save(user3);
-            userServiceImp.save(user4);
-            userServiceImp.save(user5);
-            userServiceImp.save(user6);
-            userServiceImp.save(user7);
-            userServiceImp.save(user8);
-            userServiceImp.save(user9);
-            userServiceImp.save(user10);
         }
     }
 }
